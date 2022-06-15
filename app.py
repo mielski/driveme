eaba794fed6853ae745f6419470bdf2511a42106
happi <happiemiel@gmail.com>
2022-06-15 23:08:52 +0200

    get data on events from database ("local")

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
@@ -6,13 +8,35 @@ load_dotenv()
 app = Flask(__name__)
 
 
+events = [
+    {"title": "Teddybear hospital",
+     "desc": "Leer over het ziekenhuis en laat je knuffel beter worden in het ziekenhuis.",
+     "date": datetime.date(2020, 6, 26),
+     "start": "9:00",
+     "end": "11:00",
+     "cars": 24,
+     "needed": 27,
+     },
+    {"title": "Oud Valkeveen",
+     "desc": "Ga mee met de draak of neem een duik.",
+     "date": datetime.date(2020, 7, 31),
+     "start": "9:00",
+     "end": "14:15",
+     "cars": 12,
+     "needed": 27,
+     }
+]
 
+def get_date_elements(date: datetime.date):
 
+    return (date.strftime("%Y-%m-%d"), date.strftime("%d %B"))
 
 @app.route('/')
 def home():  # put application's code here
 
-    return render_template("home.html", title="Drive me")
+    for event in events:
+        event["isodate"], event["shortdate"] = get_date_elements(event["date"])
+    return render_template("home.html", title="Drive me", events=events)
 
 
 @app.route('/register')
